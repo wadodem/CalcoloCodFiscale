ad7fb1ee3f88ebfe781bb980ee966e40283169e9
wadodem <83982173+wadodem@users.noreply.github.com>
2021-05-11 11:19:11 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,14 @@
+/*
+
+Calcolatore di codice fiscale italiano
+
+11/05/2021
+
+Giacomo Tarli
+
+ */
+
+
 import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
@@ -15,16 +26,16 @@ import java.util.*;
 
 public class Main {
 
-    //directory del file Excel contenente la lista dei comuni italiani e i corrispettivi codici di riferimento
+    //Directory del file Excel contenente la lista dei comuni italiani e i corrispettivi codici di riferimento.
     private static final String EXCEL_FILE_LOCATION = "C:\\Users\\wadod\\IdeaProjects\\CalcoloCodFisc\\src\\main\\resources\\Comuni.xls";
+
+    //Scanner per input dati.
+    static Scanner scanner = new Scanner(System.in);
 
     private static char codiceMese;
     private static int codiceDiControllo;
 
-    //scanner per input dati
-    static Scanner scanner = new Scanner(System.in);
-
-    //metodo di verifica inserimento dati corretti
+    //Metodo di verifica inserimento dati corretti.
     public static boolean checkAlfabeto(String nomeDaControllare) {
         for (int x = 0; x < nomeDaControllare.length(); x++) {
             if (nomeDaControllare.charAt(x) == ' ' || nomeDaControllare.charAt(x) == '\'') {
@@ -39,8 +50,11 @@ public class Main {
     public static void main(String[] args) {
 
 
-        //loop che richiede inserimento cognome finché non ne viene inserito uno corretto (solo caratteri dell'alfabeto)
-        //variabili che verranno riempite coi dati dell'utente
+        /*
+         Loop che richiede inserimento cognome finché non ne viene inserito uno
+         corretto (solo caratteri dell'alfabeto)
+         */
+
         String cognome;
         while (true) {
             try {
@@ -48,23 +62,28 @@ public class Main {
 
                 cognome = scanner.nextLine();
 
-                //controlla che il cognome sia inserito correttamente (che sia inserito qualcosa, solo caratteri dell'alfabeto)
+                //Controlla che venga inserita una stringa come cognome e che essa contenga solo caratteri dell'alfabeto.
                 if (cognome.isEmpty() || checkAlfabeto(cognome)) {
                     System.out.println("Inserire un cognome valido!");
                     continue;
                 }
 
-                //modifica il cognome rendendolo maiuscolo
+                //Modifica il cognome rendendolo maiuscolo.
                 cognome = cognome.toUpperCase();
 
-                //rimozione di eventuali spazi, apostrofi e accenti per cognomi come "De Luca", "D'Annunzio", "Polà"
+                /*Rimozione di eventuali spazi, apostrofi e accenti per cognomi come "De Luca", "D'Annunzio",
+                 "Polà".
+                 */
                 cognome = cognome.replaceAll("\\s", "");
                 cognome = cognome.replaceAll("'", "");
                 cognome = Normalizer.normalize(cognome, Normalizer.Form.NFD);
                 cognome = cognome.replaceAll("\\p{M}", "");
 
 
-                //creazione della parte di codice fiscale relativo al cognome
+
+                /*Creazione della parte di codice fiscale relativo al cognome
+                  in base a quante consonanti e vocali contiene.
+                 */
                 String consonantiCognome = cognome.replaceAll("[aeiouAEIOU]", "");
                 String vocaliCognome = cognome.replaceAll("[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]", "");
 
@@ -87,18 +106,19 @@ public class Main {
                     } else if (vocaliCognome.length() == 2) {
                         cognome = vocaliCognome.substring(0, 2) + 'X';
                     } else if (vocaliCognome.length() == 1) {
-                        cognome = vocaliCognome.substring(0, 1) + "XX";
+                        cognome = vocaliCognome.charAt(0) + "XX";
                     }
                 }
 
                 break;
             } catch (Exception e) {
                 e.printStackTrace();
+                continue;
             }
         }
 
 
-        //loop che richiede inserimento nome finché non ne viene inserito uno corretto (solo caratteri dell'alfabeto)
+        //Loop che richiede inserimento nome; come sopra per il cognome.
         String nome;
         while (true) {
             try {
@@ -112,16 +132,14 @@ public class Main {
                     continue;
                 }
 
-                //modifica il nome rendendolo maiuscolo
+                //Modifica il nome rendendolo maiuscolo e rimuovendo eventuali spazi, apostrofi, accenti.
                 nome = nome.toUpperCase();
-
-                //rimozione di eventuali spazi per chi ha due nomi
                 nome = nome.replaceAll("\\s", "");
                 nome = nome.replaceAll("'", "");
                 nome = Normalizer.normalize(nome, Normalizer.Form.NFD);
                 nome = nome.replaceAll("\\p{M}", "");
 
-                //creazione della parte di codice fiscale relativo al cognome
+                //Calcolo di parte di codice fiscale relativa al nome.
                 String consonantiNome = nome.replaceAll("[aeiouAEIOU]", "");
                 String vocaliNome = nome.replaceAll("[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]", "");
 
@@ -149,29 +167,32 @@ public class Main {
                 break;
             } catch (Exception e) {
                 e.printStackTrace();
+                continue;
             }
         }
 
-
+        /*Inserimento sesso. In base ad esso cambierà la parte di codice relativa
+          al giorno di nascita.
+         */
         String sesso;
         while (true) {
-                System.out.print("Inserire il sesso (M o F): ");
-                sesso = scanner.nextLine();
-                sesso = sesso.replaceAll("\\s", "");
+            System.out.print("Inserire il sesso (M o F): ");
+            sesso = scanner.nextLine();
+            sesso = sesso.replaceAll("\\s", "");
 
-                //controlla che il sesso sia inserito correttamente
-                if (sesso.equalsIgnoreCase("m") || sesso.equalsIgnoreCase("f")) {
-                    break;
-                }
-                else{
-                    System.out.println("Inserire un sesso valido!");
-                }
+            //controlla che il sesso sia inserito correttamente
+            if (sesso.equalsIgnoreCase("m") || sesso.equalsIgnoreCase("f")) {
+                break;
+            } else {
+                System.out.println("Inserire un sesso valido!");
+            }
         }
 
+        //Solo il formato gg/mm/aaaa è richiesto e accettato nell'inserimento data di nascita
         int giornoDiNascita;
         int annoDiNascita;
         int meseDiNascita;
-        while(true){
+        while (true) {
             System.out.print("Inserire la data di nascita nel formato gg/mm/aaaa: ");
             String dataDiNascita = scanner.nextLine();
             DateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
@@ -179,42 +200,39 @@ public class Main {
             Date data;
             Calendar calendar;
 
+            try {
+                data = formatoData.parse(dataDiNascita);
+                calendar = Calendar.getInstance();
+                calendar.setTime(data);
+                if (calendar.get(Calendar.YEAR) < 1901) {
+                    System.out.println("Inserire una data valida e nel formato giusto!");
+                    scanner.reset();
+                    continue;
+                }
 
-try {
-    data = formatoData.parse(dataDiNascita);
-    calendar = Calendar.getInstance();
-    calendar.setTime(data);
-    if(calendar.get(Calendar.YEAR) < 1901){
-        System.out.println("Inserire una data valida e nel formato giusto!");
-        scanner.reset();
-        continue;
-    }
+            } catch (ParseException pe) {
+                System.out.println("Inserire un formato data valido!");
+                scanner.reset();
+                continue;
+            }
 
-}catch (ParseException pe){
-    System.out.println("Inserire un formato data valido!");
-    scanner.reset();
-    continue;
-}
-
-
-annoDiNascita = calendar.get(Calendar.YEAR);
-String annoDueCifre = String.valueOf(annoDiNascita);
-if(annoDueCifre.length() == 4) {
-    annoDueCifre = annoDueCifre.substring(2, 4);
-    annoDiNascita = Integer.parseInt(annoDueCifre);
-}
-meseDiNascita = calendar.get(Calendar.MONTH);
-meseDiNascita = meseDiNascita + 1;
-giornoDiNascita = calendar.get(Calendar.DAY_OF_MONTH);
-
-
-
+            annoDiNascita = calendar.get(Calendar.YEAR);
+            String annoDueCifre = String.valueOf(annoDiNascita);
+            if (annoDueCifre.length() == 4) {
+                annoDueCifre = annoDueCifre.substring(2, 4);
+                annoDiNascita = Integer.parseInt(annoDueCifre);
+            }
+            meseDiNascita = calendar.get(Calendar.MONTH);
+            meseDiNascita = meseDiNascita + 1;
+            giornoDiNascita = calendar.get(Calendar.DAY_OF_MONTH);
 
             break;
 
         }
 
-
+        /*In base al mese di nascita viene assegnato un codice di una lettera
+          che andrà a far parte del codice fiscale.
+         */
         switch (meseDiNascita) {
             case 1 -> codiceMese = 'A';
             case 2 -> codiceMese = 'B';
@@ -230,8 +248,9 @@ giornoDiNascita = calendar.get(Calendar.DAY_OF_MONTH);
             case 12 -> codiceMese = 'T';
         }
 
+        //Inserimento comune di nascita.
         String comune;
-        while(true) {
+        while (true) {
 
             System.out.print("Inserire il comune italiano di nascita: ");
 
@@ -240,12 +259,14 @@ giornoDiNascita = calendar.get(Calendar.DAY_OF_MONTH);
             if (comune.isEmpty() || checkAlfabeto(comune)) {
                 System.out.println("Inserisci un comune valido!");
                 continue;
-            }
-            else{
+            } else {
                 comune = WordUtils.capitalize(comune);
             }
 
-            //viene utilizzata JExcelAPI per leggere il file .xls esterno contente l'elenco dei comuni ed i relativi codici di riferimento
+            /*
+             Viene utilizzata JExcelAPI per leggere il file .xls esterno contente
+             l'elenco dei comuni ed i relativi codici di riferimento.
+             */
             Workbook workbook = null;
             try {
 
@@ -258,14 +279,10 @@ giornoDiNascita = calendar.get(Calendar.DAY_OF_MONTH);
                 comune = cell2.getContents();
 
 
-
-
-            }catch (NullPointerException npe){
+            } catch (NullPointerException npe) {
                 System.out.println("Inserisci un comune valido!");
                 continue;
-            }
-
-            catch (IOException | BiffException e) {
+            } catch (IOException | BiffException e) {
                 e.printStackTrace();
             } finally {
 
@@ -273,84 +290,86 @@ giornoDiNascita = calendar.get(Calendar.DAY_OF_MONTH);
                     workbook.close();
                 }
             }
-break;
+            break;
         }
 
-
+        //Parte di cod. fiscale relativo al giorno di nascita.
         String giorno;
-        if(sesso.equalsIgnoreCase("m")) {
-                    giorno = String.format("%02d", giornoDiNascita);
-                }else {
-                    giorno = String.valueOf(giornoDiNascita)+40;
-                }
+        if (sesso.equalsIgnoreCase("m")) {
+            giorno = String.format("%02d", giornoDiNascita);
+        } else {
+            giorno = String.valueOf(giornoDiNascita) + 40;
+        }
+
         String codiceFiscale = cognome + nome + annoDiNascita + codiceMese + giorno + comune;
 
-                for(int x = 0; x < codiceFiscale.length(); x += 2){
-                    switch (codiceFiscale.charAt(x)) {
-                        case 'A', '0' -> codiceDiControllo += 1;
-                        case 'B', '1' -> {
-                        }
-                        case 'C', '2' -> codiceDiControllo += 5;
-                        case 'D', '3' -> codiceDiControllo += 7;
-                        case 'E', '4' -> codiceDiControllo += 9;
-                        case 'F', '5' -> codiceDiControllo += 13;
-                        case 'G', '6' -> codiceDiControllo += 15;
-                        case 'H', '7' -> codiceDiControllo += 17;
-                        case 'I', '8' -> codiceDiControllo += 19;
-                        case 'J', '9' -> codiceDiControllo += 21;
-                        case 'K' -> codiceDiControllo += 2;
-                        case 'L' -> codiceDiControllo += 4;
-                        case 'M' -> codiceDiControllo += 18;
-                        case 'N' -> codiceDiControllo += 20;
-                        case 'O' -> codiceDiControllo += 11;
-                        case 'P' -> codiceDiControllo += 3;
-                        case 'Q' -> codiceDiControllo += 6;
-                        case 'R' -> codiceDiControllo += 8;
-                        case 'S' -> codiceDiControllo += 12;
-                        case 'T' -> codiceDiControllo += 14;
-                        case 'U' -> codiceDiControllo += 16;
-                        case 'V' -> codiceDiControllo += 10;
-                        case 'W' -> codiceDiControllo += 22;
-                        case 'X' -> codiceDiControllo += 25;
-                        case 'Y' -> codiceDiControllo += 24;
-                        case 'Z' -> codiceDiControllo += 23;
-                    }
+        //Calcolo dell'ultimo carattere del codice fiscale.
+        for (int x = 0; x < codiceFiscale.length(); x += 2) {
+            switch (codiceFiscale.charAt(x)) {
+                case 'A', '0' -> codiceDiControllo += 1;
+                case 'B', '1' -> {
                 }
+                case 'C', '2' -> codiceDiControllo += 5;
+                case 'D', '3' -> codiceDiControllo += 7;
+                case 'E', '4' -> codiceDiControllo += 9;
+                case 'F', '5' -> codiceDiControllo += 13;
+                case 'G', '6' -> codiceDiControllo += 15;
+                case 'H', '7' -> codiceDiControllo += 17;
+                case 'I', '8' -> codiceDiControllo += 19;
+                case 'J', '9' -> codiceDiControllo += 21;
+                case 'K' -> codiceDiControllo += 2;
+                case 'L' -> codiceDiControllo += 4;
+                case 'M' -> codiceDiControllo += 18;
+                case 'N' -> codiceDiControllo += 20;
+                case 'O' -> codiceDiControllo += 11;
+                case 'P' -> codiceDiControllo += 3;
+                case 'Q' -> codiceDiControllo += 6;
+                case 'R' -> codiceDiControllo += 8;
+                case 'S' -> codiceDiControllo += 12;
+                case 'T' -> codiceDiControllo += 14;
+                case 'U' -> codiceDiControllo += 16;
+                case 'V' -> codiceDiControllo += 10;
+                case 'W' -> codiceDiControllo += 22;
+                case 'X' -> codiceDiControllo += 25;
+                case 'Y' -> codiceDiControllo += 24;
+                case 'Z' -> codiceDiControllo += 23;
+            }
+        }
 
-                for(int x = 1; x < codiceFiscale.length(); x += 2) {
+        for (int x = 1; x < codiceFiscale.length(); x += 2) {
 
-                    switch (codiceFiscale.charAt(x)) {
-                        case 'A', '0' -> {
-                        }
-                        case 'B', '1' -> codiceDiControllo += 1;
-                        case 'C', '2' -> codiceDiControllo += 2;
-                        case 'D', '3' -> codiceDiControllo += 3;
-                        case 'E', '4' -> codiceDiControllo += 4;
-                        case 'F', '5' -> codiceDiControllo += 5;
-                        case 'G', '6' -> codiceDiControllo += 6;
-                        case 'H', '7' -> codiceDiControllo += 7;
-                        case 'I', '8' -> codiceDiControllo += 8;
-                        case 'J', '9' -> codiceDiControllo += 9;
-                        case 'K' -> codiceDiControllo += 10;
-                        case 'L' -> codiceDiControllo += 11;
-                        case 'M' -> codiceDiControllo += 12;
-                        case 'N' -> codiceDiControllo += 13;
-                        case 'O' -> codiceDiControllo += 14;
-                        case 'P' -> codiceDiControllo += 15;
-                        case 'Q' -> codiceDiControllo += 16;
-                        case 'R' -> codiceDiControllo += 17;
-                        case 'S' -> codiceDiControllo += 18;
-                        case 'T' -> codiceDiControllo += 19;
-                        case 'U' -> codiceDiControllo += 20;
-                        case 'V' -> codiceDiControllo += 21;
-                        case 'W' -> codiceDiControllo += 22;
-                        case 'X' -> codiceDiControllo += 23;
-                        case 'Y' -> codiceDiControllo += 24;
-                        case 'Z' -> codiceDiControllo += 25;
-                    }
+            switch (codiceFiscale.charAt(x)) {
+                case 'A', '0' -> {
                 }
+                case 'B', '1' -> codiceDiControllo += 1;
+                case 'C', '2' -> codiceDiControllo += 2;
+                case 'D', '3' -> codiceDiControllo += 3;
+                case 'E', '4' -> codiceDiControllo += 4;
+                case 'F', '5' -> codiceDiControllo += 5;
+                case 'G', '6' -> codiceDiControllo += 6;
+                case 'H', '7' -> codiceDiControllo += 7;
+                case 'I', '8' -> codiceDiControllo += 8;
+                case 'J', '9' -> codiceDiControllo += 9;
+                case 'K' -> codiceDiControllo += 10;
+                case 'L' -> codiceDiControllo += 11;
+                case 'M' -> codiceDiControllo += 12;
+                case 'N' -> codiceDiControllo += 13;
+                case 'O' -> codiceDiControllo += 14;
+                case 'P' -> codiceDiControllo += 15;
+                case 'Q' -> codiceDiControllo += 16;
+                case 'R' -> codiceDiControllo += 17;
+                case 'S' -> codiceDiControllo += 18;
+                case 'T' -> codiceDiControllo += 19;
+                case 'U' -> codiceDiControllo += 20;
+                case 'V' -> codiceDiControllo += 21;
+                case 'W' -> codiceDiControllo += 22;
+                case 'X' -> codiceDiControllo += 23;
+                case 'Y' -> codiceDiControllo += 24;
+                case 'Z' -> codiceDiControllo += 25;
+            }
+        }
 
-                codiceDiControllo = codiceDiControllo%26;
+        codiceDiControllo = codiceDiControllo % 26;
 
         switch (codiceDiControllo) {
             case 0 -> codiceFiscale = codiceFiscale.concat("A");
@@ -381,6 +400,6 @@ break;
             case 25 -> codiceFiscale = codiceFiscale.concat("Z");
         }
 
-                System.out.println("Codice Fiscale: " + codiceFiscale);
-            }
-        }
+        System.out.println("Codice Fiscale: " + codiceFiscale);
+    }
+}
